f997656f843e6b8735bf92bb55dbfeaf7bee25a9
yarixer0 <yarixer0@gmail.com>
2025-01-02 19:36:48 +0100

    start commit

---
+++ src/main/java/org/yanix/yx_core/Yx_core.java
@@ -0,0 +1,17 @@
+package org.yanix.yx_core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Yx_core extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
